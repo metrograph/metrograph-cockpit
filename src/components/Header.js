d6eb65a4da1d192a5512a44db328be3ee8acf065
annouri <annouri.ilias@gmail.com>
2022-03-18 00:35:02 +0100

    Adding avatar +Logout  to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useNavigate } from "react-router-dom";
+import avatar from "../assets/avatar.jpg"
 
 export default function Header(props) {
+  const navigate = useNavigate()
+  function logout() {
+
+    localStorage.removeItem("localState")
+    return navigate("/")
+  }
   return (
-    <div className="bg-brand-header h-28 grid place-items-center mb-14 font-title">
+    <div className="bg-brand-header h-28  mb-14 font-title flex items-center  justify-between relative">
+      <div className="h-12 w-12 mr-20"></div>
       <Link to="/">
         <img src={props.logo} className="w-44" />
       </Link>
+      <div className="flex items-center mr-28">
+        <div className="h-12 w-12 cursor-pointer mr-4">
+
+          <div className=" flex w-full h-full items-end">
+            <img className=" flex  bg-white w-full h-full" src={avatar} />
+            <div className=" border-l-2 h-10  border-cock-purple" />
+          </div>
+          <div className="border-b-2 border-cock-purple"></div>
+
+        </div>
+        <p className="text-md text-white font-Rajdhani font-medium cursor-pointer hover:text-orange-500" onClick={() => logout()}>
+          Logout
+        </p>
+      </div>
+
     </div>
   );
 }

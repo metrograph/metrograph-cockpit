ed57f96b5e803c0a17862c49e39241136e5b77cd
annouri ilias <annouri.ilias@gmail.com>
2022-04-29 01:50:33 +0000

    cursor map

--- src/screens/dev/EditAction.js
+++ src/screens/dev/EditAction.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { render } from "react-dom";
+import React, { useEffect, useState } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/snippets/python";
 import { ReactComponent as ActionIcon } from "../../assets/topbar/action.svg";
@@ -14,6 +15,7 @@ import { ReactComponent as WorkflowsIcon } from "../../assets/topbar/workflows.s
 import { ReactComponent as ArrowDown } from "../../assets/icons/arrow-down.svg";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import pythonIcon from "../../assets/python.svg";
+import useMouse from "@react-hook/mouse-position";
 
 import i_icon from "../../assets/icons/i.svg";
 
@@ -64,15 +66,27 @@ export default function CreateAction() {
 
   const [urlCheckBox, setUrlCheckBox] = useState(true);
   let dt = { username: "ehamza", password: "123" };
-  const [output, setOutput] = useState(JSON.stringify(dt, null, "\t"));
-
+  const [eventIDE, setEventIDE] = useState(JSON.stringify(dt, null, "\t"));
+  const [outputIDE, setOutputIDE] = useState("");
+  const [responseIDE, setResponseIDE] = useState("");
+  const [panelMenu, setPanelMenu] = useState(false);
+  const [mouseRadar, setMouseRadar] = useState({ x: "0", y: "0" });
   function handleClick(e) {
     if (e.type === "click") {
-      console.log("Left click");
+      setPanelMenu(false);
     } else if (e.type === "contextmenu") {
-      console.log("Right click");
+      console.log(e.clientX, e.clientY);
+      setMouseRadar({ x: mouse.x, y: mouse.x });
+
+      setPanelMenu(!panelMenu);
     }
   }
+  const ref = React.useRef(null);
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+  useEffect(() => {}, []);
 
   return (
     <div className="bg-black min-h-screen noselect flex justify-center pb-24 px-12">
@@ -403,25 +417,55 @@ export default function CreateAction() {
           <div className=" font-IBM-Plex-Sans font-bold text-[11px] mb-[10px] text-white">
             ACTION CODE
           </div>
-          <div className="flex h-[427px] bg-[#202020] ">
-            <div className="bg-[#202020] w-1/5">
+          <div className="flex h-[427px] bg-[#202020]">
+            {/*Lef panel section*/}
+            <div
+              onContextMenu={(e) => e.preventDefault()}
+              className="bg-[#202020] w-1/5 relative"
+              ref={ref}
+            >
               <div
                 onClick={handleClick}
                 onContextMenu={handleClick}
-                className="mt-[30px] flex items-center ml-[42px] space-x-[7px] cursor-pointer"
+                className="cursor-pointer h-full"
               >
-                <ArrowDown />
-                <div className="text-[11px] text-white font-IBM-Plex-Sans font-bold ">
-                  FILES
+                <div className="mt-[30px] flex items-center ml-[42px] space-x-[7px] ">
+                  <ArrowDown />
+                  <div className="text-[11px] text-white font-IBM-Plex-Sans font-bold ">
+                    FILES X{mouseRadar.x} Y{mouseRadar.y}
+                  </div>
+                </div>
+                <div className="flex items-center space-x-[9px] ml-[49px] mt-[15px] cursor-pointer">
+                  <img src={pythonIcon} className="w-[13px] h-[13px]" alt="" />
+                  <div className="text-white font-IBM-Plex-Sans text-[12px] font-medium">
+                    main.py
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-[9px] ml-[49px] mt-[15px] cursor-pointer">
-                <img src={pythonIcon} className="w-[13px] h-[13px]" alt="" />
-                <div className="text-white font-IBM-Plex-Sans text-[12px] font-medium">
-                  main.py
+              {panelMenu && (
+                <div
+                  className="bg-[#191919] w-[187px] h-[89px] rounded-[9px] flex flex-col justify-center  border-1 border-[#292929] space-y-[1px]"
+                  style={{ position: "absolute", top: mouse.y, left: mouse.x }}
+                >
+                  <div className="text-white font-IBM-Plex-Sans font-bold text-[9px] hover:bg-[#292929] pl-[13px] cursor-pointer">
+                    CREATE FOLDER
+                  </div>
+                  <div className="text-white font-IBM-Plex-Sans font-bold text-[9px] hover:bg-[#292929] pl-[13px] cursor-pointer">
+                    CREATE FILE
+                  </div>
+                  <div className="text-white font-IBM-Plex-Sans font-bold text-[9px] hover:bg-[#292929] pl-[13px] cursor-pointer">
+                    UPLOAD FILE
+                  </div>
+                  <div className="text-white font-IBM-Plex-Sans font-bold text-[9px] hover:bg-[#292929] pl-[13px] cursor-pointer">
+                    RENAME
+                  </div>
+                  <div className="text-white font-IBM-Plex-Sans font-bold text-[9px] hover:bg-[#292929] pl-[13px] cursor-pointer">
+                    DELETE
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
+            {/*Lef panel section end*/}
             <div className="w-4/5">
               <div className="bg-[#202020] w-[171px] h-[53px] flex items-center justify-center space-x-[9px] border-b-4 border-[#7900FF]">
                 <img src={pythonIcon} className="w-[14px] h-[14px]" alt="" />
@@ -433,10 +477,10 @@ export default function CreateAction() {
                 <AceEditor
                   showPrintMargin={false}
                   height="374px"
-                  fontSize="14px"
+                  fontSize="17px"
                   width="100%"
                   mode="python"
-                  theme="monokai"
+                  theme="tomorrow_night"
                   onChange={(value) => console.log(value)}
                   name="ace_firts"
                   editorProps={{ $blockScrolling: true }}
@@ -467,10 +511,11 @@ export default function CreateAction() {
                   fontSize="14px"
                   width="100%"
                   mode="json"
-                  theme="monokai"
-                  onChange={(value) => setOutput(value)}
-                  value={output}
+                  theme="twilight"
+                  onChange={(value) => setEventIDE(value)}
+                  value={eventIDE}
                   name="ace_firts"
+                  showGutter={false}
                   editorProps={{ $blockScrolling: true }}
                   setOptions={{
                     enableBasicAutocompletion: true,
@@ -488,8 +533,67 @@ export default function CreateAction() {
                 </div>
               </div>
             </div>
-            <div className="w-2/5 border-r-[3px] border-[#343434] "></div>
-            <div className="w-2/5"></div>
+            <div className="w-2/5 border-r-[3px] border-[#343434] px-[19px]">
+              <div className="mt-[19px]  flex space-x-[11px]">
+                <div className=" font-IBM-Plex-Sans font-bold text-white text-[11px]">
+                  OUTPUT
+                </div>
+                <img src={i_icon} alt="" />
+              </div>
+              <div className="bg-[#1A1A1A] h-[178px] w-full rounded-[13px] mt-[12px] text-white text-[10px] relative">
+                <AceEditor
+                  style={{ borderRadius: "13px" }}
+                  showPrintMargin={false}
+                  height="100%"
+                  fontSize="14px"
+                  width="100%"
+                  mode="json"
+                  theme="twilight"
+                  onChange={(value) => setOutputIDE(value)}
+                  value={outputIDE}
+                  name="ace_firts"
+                  showGutter={false}
+                  editorProps={{ $blockScrolling: true }}
+                  setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: false,
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="w-2/5 px-[19px]">
+              <div className="mt-[19px]  flex space-x-[11px]">
+                <div className=" font-IBM-Plex-Sans font-bold text-white text-[11px]">
+                  RESPONSE
+                </div>
+                <img src={i_icon} alt="" />
+              </div>
+              <div className="bg-[#1A1A1A] h-[178px] w-full rounded-[13px] mt-[12px] text-white text-[10px] relative">
+                <AceEditor
+                  style={{ borderRadius: "13px" }}
+                  showPrintMargin={false}
+                  height="100%"
+                  fontSize="14px"
+                  width="100%"
+                  mode="json"
+                  theme="twilight"
+                  onChange={(value) => setResponseIDE(value)}
+                  value={responseIDE}
+                  name="ace_firts"
+                  showGutter={false}
+                  editorProps={{ $blockScrolling: true }}
+                  setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: false,
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>

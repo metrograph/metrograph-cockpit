7bd94c0f98278d782bc43efe4ab91ae363eae03b
unknown <annouri.ilias@gmail.com>
2022-09-10 16:42:42 +0100

    Custom Node implementation

--- src/screens/flow/Flow.js
+++ src/screens/flow/Flow.js
@@ -1,11 +1,45 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect, useMemo } from 'react';
 import useMouse from "@react-hook/mouse-position";
-import ReactFlow, {useReactFlow, ReactFlowProvider, Background, addEdge, applyNodeChanges, applyEdgeChanges, useKeyPress} from 'react-flow-renderer';
+import ReactFlow, {useReactFlow, ReactFlowProvider, Background, addEdge, applyNodeChanges, applyEdgeChanges, useKeyPress, Handle, Position} from 'react-flow-renderer';
+import githubIcon from "../../assets/icons/github.svg"
+function TextUpdaterNode({ data }) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
 
+  return (
+    <>
+      <Handle type="target" position={Position.Top} />
+      <div className='h-[99px] w-[99px] flex items-center relative'>
+        <div className='bg-white h-[99px] w-[99px] grid place-items-center rounded-[25px] border-2 border-[#D8D8D8]'>
+          <img src={githubIcon} className="w-[49px] h-[50px]" alt="github-logo" />
+        </div>
+        <div className='absolute left-28 w-[123px]'>
+          <div className=' font-IBM-Plex-Sans font-bold text-[15px]'>
+          Github
+          </div>
+          <div className=' font-IBM-Plex-Sans font-medium text-[12px] text-[#838383]'>
+          Commit Trigger [main]
+          </div>
+          <div className='font-IBM-Plex-Sans font-bold text-[9px] text-[#7ECA9C]'>
+          Successful Test
+          </div>
+        </div>
+      </div>
+      <Handle type="source" position={Position.Bottom} id="a" />
+    </>
+  );
+}
 
+function CostumNode(){
+  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
+
+return <ReactFlow nodeTypes={nodeTypes} />;
+}
 
 function Flow() {
-  const [nodes, setNodes] = useState([]);
+  const nodeTypes = { textUpdater: TextUpdaterNode };
+  const [nodes, setNodes] = useState([ { id: 'node-1', type: 'textUpdater', position: { x: 0, y: 0 }, data: { value: 123 } }]);
   const [edges, setEdges] = useState([]);
   const [timer,setTimer]=useState(null)
   const [position, setPosition]= useState({x:0, y:0})
@@ -14,6 +48,8 @@ function Flow() {
   const reactFlowInstance = useReactFlow();
   const ref = React.useRef(null);
   const cmdAndSPressed = useKeyPress(['Meta+d','d', 'Strg+d', 'Delete']);
+
+ 
   
   const onNodesChange = useCallback(
     (changes) => {setNodes((nds) => applyNodeChanges(changes, nds))},
@@ -85,6 +121,7 @@ function Flow() {
       className={addnodeMode?"cursor-cell":"cursor-pointer"}
       onClick={()=>handleClick()}
       onContextMenu={(e) =>{e.preventDefault();setContextMenu(!contextMenu)}}
+      nodeTypes={nodeTypes}
       ref={ref}
       nodes={nodes}
       edges={edges}
